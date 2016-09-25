e83eac91de0296c008f062e59fda623b8344cc59
Jonas Petersen <je77petersen@gmail.com>
2016-09-25 12:12:44 +0200

    init Calculator class init SensorValue class

---
+++ Caelum/app/src/main/java/com/nyxxstudios/caelum/Calculator.java
@@ -0,0 +1,13 @@
+package com.nyxxstudios.caelum;
+
+/**
+ * Created by Jonas on 25.09.2016.
+ */
+public class Calculator {
+
+    //this method is called every X seconds and saves all current sensor values
+    //
+    public static void logData(){
+
+    }
+}
---
+++ Caelum/app/src/main/java/com/nyxxstudios/caelum/SensorValue.java
@@ -0,0 +1,18 @@
+package com.nyxxstudios.caelum;
+
+import java.util.Date;
+
+/**
+ * Created by Jonas on 25.09.2016.
+ */
+public class SensorValue {
+    private Date time;
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+}
