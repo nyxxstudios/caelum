2ea72db499cf83ba99820354c3c29ed1de739b6c
gordonkoehn <gordon@koehn.net>
2016-09-25 22:27:26 +0200

    finished getPressure() and getAcceleration() - Testing needed

--- Caelum/app/src/main/java/com/nyxxstudios/caelum/Sensors.java
+++ Caelum/app/src/main/java/com/nyxxstudios/caelum/Sensors.java
@@ -22,11 +22,18 @@ public class Sensors implements SensorEventListener {
         Sensor presSensor;
         SensorManager presSM;
 
+        SensorEvent event = null;
+        Sensor sensor = event.sensor;
+
         float pressure = 0;
         //get our Sensor Mananger
         presSM = (SensorManager) mainActivity.getSystemService(Context.SENSOR_SERVICE);
         //Assign Sensors
         presSensor = presSM.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        //register Listener
+        presSM.registerListener(this, presSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        pressure = event.values[0];
 
         return pressure;
     }
@@ -35,11 +42,21 @@ public class Sensors implements SensorEventListener {
         Sensor accSensor;
         SensorManager accSM;
 
+        SensorEvent event = null;
+        Sensor sensor = event.sensor;
+
         double[] acceleration = new double[3];
         //get our Sensor Mananger
         accSM = (SensorManager) mainActivity.getSystemService(Context.SENSOR_SERVICE);
         //Assign Sensors
         accSensor = accSM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //register Listener
+        accSM.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        for (int i = 0; i <= 2; i++) {
+            acceleration[i] = event.values[i];
+        }
+
         return acceleration;
     }
 
