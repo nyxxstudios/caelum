243bb50b7d7686ea7351ba97a53b16eb12be89b7
Jonas Petersen <je77petersen@gmail.com>
2016-09-25 12:44:55 +0200

    write constructor

--- Caelum/app/src/main/java/com/nyxxstudios/caelum/SensorValue.java
+++ Caelum/app/src/main/java/com/nyxxstudios/caelum/SensorValue.java
@@ -6,31 +6,36 @@ import java.util.Date;
  * Created by Jonas on 25.09.2016.
  */
 public class SensorValue {
+    //all 'sensor variables' don't need a setter, because they are just once defined
+    // (in the constructor of this class) and their value is never changed from outside of this class
+
+
     private Date time; //time of creation
     public Date getTime() {
         return time;
     }
-    public void setTime(Date time) {
-        this.time = time;
-    }
 
 
     private double[] accelerationValues = new double[3]; //X, Y and Z
-    public void setAccelerationValues(double[] accelerationValues) {
-        this.accelerationValues = accelerationValues;
-    }
     public double[] getAccelerationValues() {
         return accelerationValues;
     }
 
 
-    private double pressure;
+    private double pressure; //current air pressure
     public double getPressure() {
         return pressure;
     }
-    public void setPressure(double pressure) {
-        this.pressure = pressure;
+
+    private double heigth;//is calculated based on the current pressure
+    public double getHeigth() {
+        return heigth;
     }
 
+
+    //constructor. Defines variables with sensor values
+    public SensorValue(){
+
+    }
 
 }
