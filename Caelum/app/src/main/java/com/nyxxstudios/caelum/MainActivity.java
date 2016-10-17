b3de5c7c0d1025d3342341b20f1184b42f5fb89c
gordonkoehn <gordon@koehn.net>
2016-10-17 16:36:14 +0200

    unfinished GPS impementation ... with errors

--- Caelum/app/src/main/java/com/nyxxstudios/caelum/MainActivity.java
+++ Caelum/app/src/main/java/com/nyxxstudios/caelum/MainActivity.java
@@ -15,6 +15,19 @@ import android.hardware.SensorManager;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 
+//GPS classes
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 
 //UI classes
 import android.widget.Button;
@@ -28,6 +41,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager mSensorManager;
     private Sensor mPressure;
     private Sensor mAcceleration;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     public static float getCurrentPressure() {
         return currentPressure;
@@ -38,6 +53,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         return currentAcceleration;
     }
     private static float[] currentAcceleration = new float[3];
+
+    public static double[] currentLocation = new double[2];
+    public static double[] getCurrentLocation(){return currentLocation; }
+
+
+    Button btnStart;
 
 
     @Override
@@ -56,7 +77,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
         });
 
-        Button btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart = (Button) findViewById(R.id.btnStart);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,6 +86,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 }else{
                     System.err.println("pressureAtGround not set correctly!");
                 }
+                configure_button();
                 startLogging();
             }
         });
@@ -72,6 +94,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         //get our Sensor Mananger
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        //get Location Mananger
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         //Assign Sensors
         mAcceleration = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -90,6 +115,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         //Register Sensor Listener
         mSensorManager.registerListener(this, mAcceleration, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(this, mPressure,SensorManager.SENSOR_DELAY_NORMAL);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                currentLocation[1] = location.getLongitude();
+                currentLocation[2] = location.getLatitude();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
 
     }
 
@@ -160,5 +210,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }.start();
     }
 
+    //GPS Permission not granted
+
+  //**  @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    void configure_button(){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        //noinspection MissingPermission
+        locationManager.requestLocationUpdates("gps", 1000, 0, locationListener);
+
+    }
 
 }
